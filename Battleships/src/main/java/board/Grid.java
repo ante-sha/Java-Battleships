e780951810582bc11a374a2bec251b0c240cc43b
minja <minja.malovic@33barrage.com>
2018-07-21 10:45:39 +0200

    header added

--- Battleships/src/main/java/board/Grid.java
+++ Battleships/src/main/java/board/Grid.java
@@ -1,17 +1,22 @@
 package game;
+import java.io.*;
+import java.util.*;
 
 public class Grid {
 
     // Instance Variables
 
-
+    private String[] xAxis = {"A","B","C","D","E","F","G","H","I","J"};
+    private int[] yAxis = {1,2,3,4,5,6,7,8,9,10};
 
     // Constructor
 
     public Grid () {
-
+        System.out.println(Arrays.toString(xAxis));
+        System.out.println(Arrays.toString(yAxis));
     }
 
     // Method
+
 
 }
