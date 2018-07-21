d1f78f6d031e3a802d77ce7cf4b74c34749bf60c
minja <minja.malovic@33barrage.com>
2018-07-21 13:55:11 +0200

    Grid class updated Added Square class Draw grid added

--- Battleships/src/main/java/board/Grid.java
+++ Battleships/src/main/java/board/Grid.java
@@ -1,4 +1,5 @@
 package game;
+
 import java.io.*;
 import java.util.*;
 
@@ -6,17 +7,71 @@ public class Grid {
 
     // Instance Variables
 
-    private String[] xAxis = {"A","B","C","D","E","F","G","H","I","J"};
-    private int[] yAxis = {1,2,3,4,5,6,7,8,9,10};
-
+    private String[] xAxis = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+    private int[] yAxis = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    private HashMap<Integer, Square> grid = new HashMap<Integer, Square>();
     // Constructor
 
-    public Grid () {
+    public Grid() {
         System.out.println(Arrays.toString(xAxis));
         System.out.println(Arrays.toString(yAxis));
+        createGrid();
     }
 
     // Method
+    private void createGrid() {
+        int counter = 0;
+        for (int i = 0; i < this.xAxis.length; i++) {
 
+            for (int j = 0; j < this.yAxis.length; j++) {
+                counter++;
+                this.grid.put(counter, new Square(this.xAxis[i], this.yAxis[j]));
+            }
+        }
+    }
+
+    public void drawGrid() {
+        drawGridHeader();
+        int counter = 0;
+        int row = 0;
+        for (Map.Entry squareAtt : this.grid.entrySet()) {
+
+            if (counter >= 9) {
+                System.out.println("| |");
+                counter = 0;
+                row++;
+            } else {
+                if(counter == 0){
+                    if(this.yAxis[row]<10){
+                        System.out.print( this.yAxis[row] + " | ");
+                    }else {
+                        System.out.print( this.yAxis[row] + "| ");
+                    }
+
+                }else {
+                    System.out.print("| ");
+                }
+                counter++;
+            }
+        }
+    }
+
+    private void drawGridHeader() {
+        String header = "  ";
+        String headerDelimiter = "";
+        for (String headerAtt : this.xAxis) {
+            header = header + "|" + headerAtt ;
+            headerDelimiter = headerDelimiter + "+—";
+            if(this.xAxis[this.xAxis.length-1]==headerAtt){
+                header = header + "|" ;
+                headerDelimiter = headerDelimiter + "+—+";
+            }
+
+        }
+        System.out.println(header);
+        System.out.println(headerDelimiter);
+
+    }
 
 }
+
