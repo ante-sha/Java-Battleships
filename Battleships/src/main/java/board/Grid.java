1be842e1666da26199be4a14efcd4c149b9a6daa
michael.widmann <michael.widmann@33barrage.com>
2018-08-02 09:47:14 +0200

    Add Javadoc and an example how to use it

--- Battleships/src/main/java/board/Grid.java
+++ Battleships/src/main/java/board/Grid.java
@@ -3,50 +3,77 @@ package board;
 import java.io.*;
 import java.util.*;
 
-public class Grid {
-
-    // Instance Variables
-
+/**
+ * This class creates a grid (10 x 10 squares) for the battleship game.
+ * The grid contains a hundred squares and each square has its own unique number
+ * between 1 to 100.
+ * The x-axis of the grid contains letters A to J, the y-axis has
+ * numbers 1 to 10.
+ * @author     Minja Malovic
+ * @author     Ivan Uglik
+ * @version    1.0 - 02.08.2018
+ * @since      1.0
+ */
+public class Grid
+{
     private String[] xAxis = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
     private int[] yAxis = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    private HashMap<Integer, Square> grid = new HashMap<Integer, Square>();
-    // Constructor
+    private HashMap<Integer, Square> grid = new HashMap<>();
 
-    public Grid() {
+    /**
+     * The constructor creates an empty grid in form
+     * of a HashMap where the keys are unique numbers (1 to 100) and values
+     * are Square objects.
+     */
+    public Grid()
+    {
         createGrid();
     }
 
-    // Method
-    private void createGrid() {
+    private void createGrid()
+    {
         int counter = 0;
-        for (int i = 0; i < this.xAxis.length; i++) {
-
-            for (int j = 0; j < this.yAxis.length; j++) {
+        for (int i = 0; i < this.xAxis.length; i++)
+        {
+            for (int j = 0; j < this.yAxis.length; j++)
+            {
                 counter++;
                 this.grid.put(counter, new Square(this.xAxis[i], this.yAxis[j]));
             }
         }
     }
 
-    public void drawGrid() {
+    /**
+     * This method draws a simple grid for the command line using <code>System.out.print</code>.
+     */
+    public void drawGrid()
+    {
         drawGridHeader();
         int counter = 0;
         int row = 0;
-        for (Map.Entry squareAtt : this.grid.entrySet()) {
-
-            if (counter >= 9) {
+        for (Map.Entry squareAttr : this.grid.entrySet())
+        {
+            if (counter >= 9)
+            {
                 System.out.println("| |");
                 counter = 0;
                 row++;
-            } else {
-                if(counter == 0){
-                    if(this.yAxis[row]<10){
-                        System.out.print( this.yAxis[row] + " | ");
-                    }else {
-                        System.out.print( this.yAxis[row] + "| ");
+            }
+            else
+            {
+                if (counter == 0)
+                {
+                    if (this.yAxis[row]<10)
+                    {
+                        System.out.print(this.yAxis[row] + " | ");
                     }
-
-                }else {
+                    else
+                    {
+                        System.out.print(this.yAxis[row] + "| ");
+                    }
+                }
+                else
+                {
                     System.out.print("| ");
                 }
                 counter++;
@@ -54,37 +81,58 @@ public class Grid {
         }
     }
 
-    private void drawGridHeader() {
+    private void drawGridHeader()
+    {
         String header = "  ";
-        String headerDelimiter = "";
-        for (String headerAtt : this.xAxis) {
-            header = header + "|" + headerAtt ;
-            headerDelimiter = headerDelimiter + "+—";
-            if(this.xAxis[this.xAxis.length-1]==headerAtt){
-                header = header + "|" ;
-                headerDelimiter = headerDelimiter + "+—+";
-            }
+        String headerLine = "";
+        for (String column : this.xAxis)
+        {
+            header = header + "|" + column ;
+            headerLine = headerLine + "+—";
 
+            if (this.xAxis[this.xAxis.length-1] == column)
+            {
+                header = header + "|" ;
+                headerLine = headerLine + "+—+";
+            }
         }
         System.out.println(header);
-        System.out.println(headerDelimiter);
-
+        System.out.println(headerLine);
     }
 
-    public String[] getxAxis() {
+    /**
+     * Returns the grids x-axis.
+     *
+     * @return <code>["A", "B", "C", ...]</code>
+     */
+    public String[] getxAxis()
+    {
         return xAxis;
     }
 
-    public int[] getyAxis() {
+    /**
+     * Returns the grids y-axis.
+     *
+     * @return <code>[1, 2, 3, ...]</code>
+     */
+    public int[] getyAxis()
+    {
         return yAxis;
     }
 
-    public HashMap<Integer, Square> getGrid() {
+    /**
+     * Returns the grid.
+     *
+     * @return <code>{ 1: {xAxis: "A", yAxis: 1, ...}, 2: {xAxis: "B", yAxis: 1, ...} ...}</code>
+     */
+    public HashMap<Integer, Square> getGrid()
+    {
         return grid;
     }
 
     @Override
-    public String toString() {
+    public String toString()
+    {
         return "Grid{" +
                 "xAxis=" + Arrays.toString(xAxis) +
                 ", yAxis=" + Arrays.toString(yAxis) +
@@ -92,4 +140,3 @@ public class Grid {
                 '}';
     }
 }
-
--- Battleships/src/main/java/board/Square.java
+++ Battleships/src/main/java/board/Square.java
@@ -1,39 +1,79 @@
 package board;
+
 import java.io.*;
 import java.util.*;
 
-public class Square {
-
-    private Boolean isHit = false;
+/**
+ * This class contains the attributes of a square.
+ * Squares have fixed attributes (cooridnates) and variable
+ * attributes which change during the game.
+ * @author     Ivan Uglik
+ * @author     Minja Malovic
+ * @version    1.0 - 02.08.2018
+ * @since      1.0
+ */
+public class Square
+{
+    private Boolean wasShot = false;
     private Boolean hasShip = false;
     private String xPosition;
     private int yPosition;
 
-
-    public Square (String xPosition , int yPosition ) {
+    /**
+     * The constructor requires the coordinates as those are fixed once we've
+     * created the object.
+     * @param xPosition     The x-coordinate of the square.
+     * @param yPosition     The y-coordinate of the square.
+     */
+    public Square (String xPosition , int yPosition )
+    {
         this.xPosition = xPosition;
         this.yPosition = yPosition;
     }
 
-    @Override
-    public String toString(){
-        return this.xPosition + this.yPosition;
+    /**
+     * Changes the status if the square has been shot at.
+     *
+     * @param wasShot Set it to true if a player shot at it.
+     */ 
+    public void setWasShot(Boolean wasShot)
+    {
+        this.wasShot = wasShot;
     }
 
-
-    public void setHit(Boolean hit) {
-        this.isHit = hit;
+    /**
+     * Returns true if the square was shot at.
+     *
+     * @return true or false
+     */
+    public Boolean getWasShot()
+    {
+        return this.wasShot;
     }
 
-    public Boolean getHit() {
-        return this.isHit;
-    }
-
-    public void setHasShip(Boolean hasShip) {
+    /**
+     * Changes the status if the square is part of a vessel.
+     *
+     * @param hasShip Set it to true if a vessel was placed on this square.
+     */ 
+    public void setHasShip(Boolean hasShip)
+    {
         this.hasShip = hasShip;
     }
 
-    public Boolean getHasShip() {
+    /**
+     * Returns true if the square has a ship on it. 
+     *
+     * @return true or false
+     */
+    public Boolean getHasShip()
+    {
         return this.hasShip;
+    }
+
+    @Override
+    public String toString()
+    {
+        return this.xPosition + this.yPosition;
     }
 }
