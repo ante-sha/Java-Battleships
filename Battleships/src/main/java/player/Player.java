e2a1c7218e9fca3ec84466a826e1bfd52bd6649f
Karlo <karlo.petrovic@33barrage.com>
2018-08-03 17:05:46 +0200

    added player

---
+++ Battleships/src/main/java/player/Player.java
@@ -0,0 +1,17 @@
+package player;
+
+public class Player {
+    private String name;
+
+    public Player () {
+
+    }
+
+    public void setName (String name) {
+    this.name = name;
+    }
+
+    public String getName () {
+    return this.name;
+    }
+}
